0dde098b9667d68791ef462caf349bbf0fadfb6a
JotaFilho <j.filho-dti@hotmail.com>
2024-05-13 02:38:49 -0300

    Projeto Concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -21,9 +21,9 @@ public class ContaTerminal {
         System.out.println("Digite seu nome: ");
         String nome = scanner.next();
         
-          //imprimido os dados obtidos pelo usuario
-          System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
-          + agencia + ", conta " + numero + " e seu saldo "+ saldo + " já está disponivel para saque.");
+        //imprimido os dados obtidos pelo usuario
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + agencia + ", conta " + numero + " e seu saldo "+ saldo + " já está disponivel para saque.");
 
     }
 }
